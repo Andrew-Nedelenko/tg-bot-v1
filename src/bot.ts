f8f47e5e4e20899defd72c43b8693be608c57cac
synacten <blackhorns9999@gmail.com>
2021-11-06 12:57:31 +0200

    get data, length list

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,12 @@
 import { Telegraf } from 'telegraf';
+import { Currency } from './command/Currency';
 import { env } from './util/Env';
 
 export const bot = new Telegraf(env('BOT_TOKEN'));
 
 bot.start((ctx) => ctx.reply('Hello I am bot. Welcome home'));
+
+bot.command('/list', async (ctx) => {
+  const currencyList = await new Currency().getCurrencyList();
+  ctx.reply(currencyList.length.toString());
+});
---
+++ src/command/Currency.ts
@@ -0,0 +1,26 @@
+import { curly } from 'node-libcurl';
+import { env } from '../util/Env';
+
+type CurrencyData = {
+  StartDate: string,
+  TimeSign: string,
+  CurrencyCode: string,
+  CurrencyCodeL: string,
+  Units: number,
+  Amount: number
+}
+
+export class Currency {
+  constructor(private readonly url = `${env('NBU_URL')}/exchange?json`) {
+    this.url = url;
+  }
+
+  async getCurrencyList() {
+    try {
+      const { data } = await curly.get<CurrencyData[]>(this.url);
+      return data;
+    } catch (e) {
+      return [];
+    }
+  }
+}
